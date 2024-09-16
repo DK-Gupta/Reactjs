fc0a620df991d4597828f4740eac679f2a1af934
Dhirendra kumar gupta <dkg700731@gmail.com>
2024-09-16 14:17:53 +0530

    jsx , component - Done

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const x=7;
+  let text = "Goodbye";
+  if(x<10){
+    text = "Hello";
+  }
   return (
+    <>
     <div className="App">
-      <h1>Hello world its me DK</h1>
-      <p>
-        Hello ,there 
-      <code>Click here</code>
-      </p>
+      <h1>Hello world its me DK</h1>  
+     <h2>{text}</h2>
     </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,27 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// +++++++ Rendering simple html varable +++++++++
 
-const element = <h1>My name is Dhirendra </h1>
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-
-  (element);
-
-  <React.StrictMode>
-   <App></App>
-   
-  </React.StrictMode>
-);
-
+//  const element = <h1>My name is Dhirendra </h1>
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
+
+//  element
 // );
+
+
+//  +++++++++++ Rendering through component(App) ++++++++++
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
