5f480c1c049440fe8acf6c7f717da6808f40ce6f
Dhirendra kumar gupta <dkg700731@gmail.com>
2024-09-16 12:11:20 +0530

    sarting reactjs

--- src/index.js
+++ src/index.js
@@ -4,12 +4,27 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+const element = <h1>My name is Dhirendra </h1>
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
+  (element);
+
   <React.StrictMode>
-    <App />
+   <App></App>
+   
   </React.StrictMode>
 );
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
